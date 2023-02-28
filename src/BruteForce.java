51ed44f890b3bf8382d54fd055445c79911c2ece
Yura_Kohno <yura.kohno93@gmail.com>
2023-02-28 20:14:17 +0200

    add method BruteForce in class BruteForce

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,4 +1,23 @@
 public class BruteForce {
     private static String ALFA = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz!,.-? :\"";
 
+
+    public static void bruteForce(String message) {
+
+        for (int shift = 0; shift < 34; shift++) {
+            String decryptedMessage = "";
+            for (int i = 0; i < message.length(); i++) {
+                char c = message.charAt(i);
+                int index = ALFA.indexOf(c);
+
+                if (index != -1) {
+                    int shiftedIndex = (index - shift + ALFA.length()) % ALFA.length();
+                    decryptedMessage += ALFA.charAt(shiftedIndex);
+                } else {
+                    decryptedMessage += c;
+                }
+            }
+            System.out.println("Shift " + shift + ": " + decryptedMessage);
+        }
+    }
 }
