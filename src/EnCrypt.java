4194a5c9ca2bee72af316ec1de7c5a2cd9286a49
Yura_Kohno <yura.kohno93@gmail.com>
2023-02-28 19:36:48 +0200

    add Caesar Crypt make class Caesar Crypt add method CaesarCrypt

---
+++ src/EnCrypt.java
@@ -0,0 +1,39 @@
+public class EnCrypt {
+
+
+
+    public class CaesarCrypt {
+
+        public CaesarCrypt() {
+        }
+
+        //41 + spec symbols -rus
+        //  private static String ALFA = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя!,.-? :\"";
+        //34 + spec symbols -eng
+
+        //Add big letters
+        private static String ALFA = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz!,.-? :\"";
+
+
+
+        public static String Crypt(String message, int shift) {
+
+            //  message = message.toLowerCase();
+            String encryptStr = "";
+            for (int i = 0; i < message.length(); i++) {
+
+
+                int pos = ALFA.indexOf(message.charAt(i));
+                int encPos = (pos + shift) % 68;
+                char encChar = ALFA.charAt(encPos);
+                encryptStr += encChar;
+            }
+
+            return encryptStr;
+        }
+
+
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File src = new File("src/test.txt");
+
+        String message = new Scanner(src).useDelimiter("\\Z").next();
+
+
+
+            String encryptedMessage = EnCrypt.CaesarCipher(message, 3);
+            String decryp = EnCrypt.CaesarCipher(encryptedMessage, -3);
+
+
+            System.out.println("Crypt in: " + encryptedMessage);
+
+            System.out.println("Crypt out: " + decryp);
+
+
+
+    }
 }
